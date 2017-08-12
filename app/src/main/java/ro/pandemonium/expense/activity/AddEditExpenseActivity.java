b87938a7692d4fbf4d463b37e9b8f60f2746cbcb
goofy <barbarita>
2017-08-12 18:33:49 +0300

    replace date picker with dialog

--- app/src/main/java/ro/pandemonium/expense/activity/AddEditExpenseActivity.java
+++ app/src/main/java/ro/pandemonium/expense/activity/AddEditExpenseActivity.java
@@ -8,14 +8,19 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.*;
+
 import ro.pandemonium.expense.Constants;
 import ro.pandemonium.expense.R;
 import ro.pandemonium.expense.model.Expense;
 import ro.pandemonium.expense.model.ExpenseType;
 import ro.pandemonium.expense.view.adapter.ExpenseTypeSpinnerAdapter;
 
+import java.text.DateFormat;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
+
+import static ro.pandemonium.expense.Constants.DATE_FORMAT_PATTERN_DISPLAY;
 
 
 public class AddEditExpenseActivity extends Activity
@@ -27,13 +32,15 @@ public class AddEditExpenseActivity extends Activity
     private ExpenseTypeSpinnerAdapter expenseTypeSpinnerAdapter;
     private Spinner spinner;
     private EditText valueField;
-    private DatePicker dateField;
+    private Button dateButton;
     private EditText commentField;
 
     private Button saveExpenseButton;
 
     private final Calendar calendar = Calendar.getInstance();
+    private final DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT_PATTERN_DISPLAY, Locale.getDefault());
     private Long expenseId;
+    private long expenseTime;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,7 +48,7 @@ public class AddEditExpenseActivity extends Activity
         setContentView(R.layout.add_edit_expense);
 
         valueField = (EditText) findViewById(R.id.addExpenseValue);
-        dateField = (DatePicker) findViewById(R.id.addExpenseDatePicker);
+        dateButton = (Button) findViewById(R.id.addExpenseDate);
         spinner = (Spinner) findViewById(R.id.addExpenseSpinner);
         commentField = (EditText) findViewById(R.id.addExpenseComment);
         saveExpenseButton = (Button) findViewById(R.id.saveExpenseButton);
@@ -51,8 +58,7 @@ public class AddEditExpenseActivity extends Activity
         valueField.addTextChangedListener(this);
 
         final Intent intent = getIntent();
-        @SuppressWarnings("unchecked")
-        final HashMap<ExpenseType, Integer> mapExpenseTypeToCount = (HashMap<ExpenseType, Integer>) intent.getSerializableExtra(Constants.INTENT_EXPENSE_COUNT_MAP);
+        @SuppressWarnings("unchecked") final Map<ExpenseType, Integer> mapExpenseTypeToCount = (Map<ExpenseType, Integer>) intent.getSerializableExtra(Constants.INTENT_EXPENSE_COUNT_MAP);
         final Expense expense = (Expense) intent.getSerializableExtra(Constants.INTENT_EXPENSE_EXPENSE_TO_EDIT);
 
         expenseTypeSpinnerAdapter = new ExpenseTypeSpinnerAdapter();
@@ -75,41 +81,50 @@ public class AddEditExpenseActivity extends Activity
             valueField.setText(NUMBER_FORMATTER.format(expense.getValue()));
             spinner.setSelection(position);
             calendar.setTime(new Date(expense.getTime()));
-            dateField.updateDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+            dateButton.setText(expense.getFormattedDate(dateFormat));
             commentField.setText(expense.getComment());
+
+            expenseTime = expense.getTime();
         }
     }
 
     @Override
     public void onClick(final View view) {
-        if (view == saveExpenseButton) {
-            final Intent intent = getIntent();
+        switch (view.getId()) {
+            case R.id.saveExpenseButton:
+                final Intent intent = getIntent();
 
-            final Expense expense = new Expense();
-            expense.setId(expenseId);
-            expense.setExpenseType((ExpenseType) spinner.getSelectedItem());
-            expense.setValue(Double.parseDouble(valueField.getText().toString()));
-            expense.setDate(extractDate(dateField));
-            expense.setComment(commentField.getText().toString());
+                final Expense expense = new Expense();
+                expense.setId(expenseId);
+                expense.setExpenseType((ExpenseType) spinner.getSelectedItem());
+                expense.setValue(Double.parseDouble(valueField.getText().toString()));
+                expense.setDate(new Date(expenseTime));
+                expense.setComment(commentField.getText().toString());
 
-            intent.putExtra(Constants.INTENT_EXPENSE, expense);
+                intent.putExtra(Constants.INTENT_EXPENSE, expense);
 
-            setResult(RESULT_OK, intent);
-            finish();
+                setResult(RESULT_OK, intent);
+                finish();
+                break;
+            case R.id.addExpenseDate:
+                calendar.setTime(new Date(expenseTime));
+
+                new DatePickerDialog(this, this,
+                        calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
+                        .show();
+                break;
         }
     }
 
-    private Date extractDate(final DatePicker datePicker) {
-        calendar.set(Calendar.YEAR, datePicker.getYear());
-        calendar.set(Calendar.MONTH, datePicker.getMonth());
-        calendar.set(Calendar.DAY_OF_MONTH, datePicker.getDayOfMonth());
-
-        return calendar.getTime();
-    }
-
     @Override
-    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+    public void onDateSet(final DatePicker view, final int year, final int monthOfYear, final int dayOfMonth) {
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, monthOfYear);
+        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
 
+        Date date = calendar.getTime();
+        expenseTime = date.getTime();
+        dateButton.setText(dateFormat.format(date));
     }
 
     @Override
